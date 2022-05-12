8c83046977519425083ee262496b51f78f4c6eab
tereesaalvarez <alvdeporteresa@gmail.com>
2022-05-12 09:30:36 +0200

    commit

--- main.py
+++ main.py
@@ -2,7 +2,37 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-df= pd.read_csv('conversiones.csv')
+conversiones = pd.read_csv('conversiones.csv')
+print(conversiones)
+print('----------INTRODUCCION-------------')
+print(conversiones.columns)
+
+print('-----------VARIABLES+DATOS------------')
+print(conversiones.head())
+
+print('--------------Descrpcion conversiones------------')
+print(conversiones.describe())
+
+print('--------------informacion------------')
+conversiones.info()
+
+#contar valores
+
+pd.value_counts(conversiones['date'])
 
 
-df1= pd.read_csv('navegacion.csv')
+
+
+navegacion= pd.read_csv('navegacion.csv')
+print(navegacion)
+print('----------INTRODUCCION-------------')
+print(navegacion.columns)
+
+print('-----------VARIABLES+DATOS------------')
+print(navegacion.head())
+
+print('--------------Descrpcion conversiones------------')
+print(navegacion.describe())
+
+print('--------------informacion------------')
+navegacion.info()
