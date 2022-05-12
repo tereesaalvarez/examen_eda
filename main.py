cb33f306ab872da1ab816274837e53aeaaaa8542
tereesaalvarez <alvdeporteresa@gmail.com>
2022-05-12 10:07:32 +0200

    commit

--- main.py
+++ main.py
@@ -17,8 +17,13 @@ print('--------------informacion------------')
 conversiones.info()
 
 #contar valores
+print('-------------VALORES ID USER-------------')
+pd.value_counts(conversiones['id_user'])
+print(pd.value_counts(conversiones['id_user']))
 
-pd.value_counts(conversiones['date'])
+print('------------------VALORES GCLID-------------')
+pd.value_counts(conversiones['gclid'])
+print(pd.value_counts(conversiones['gclid']))
 
 
 
@@ -31,8 +36,35 @@ print(navegacion.columns)
 print('-----------VARIABLES+DATOS------------')
 print(navegacion.head())
 
-print('--------------Descrpcion conversiones------------')
+print('--------------Descrpcion df------------')
 print(navegacion.describe())
 
 print('--------------informacion------------')
 navegacion.info()
+
+print('-------------VALORES ID USER-------------')
+pd.value_counts(navegacion['id_user'])
+print(pd.value_counts(navegacion['id_user']))
+
+print('------------------VALORES GCLID-------------')
+pd.value_counts(navegacion['gclid'])
+print(pd.value_counts(navegacion['gclid']))
+
+print('------------------VALORES COOKIES-------------')
+pd.value_counts(navegacion['url_landing'])
+print(pd.value_counts(navegacion['url_landing']))
+
+
+# unir csv
+
+output = pd.merge(conversiones, navegacion, on='id_user', how='outer')
+print(output)
+output.to_csv('union.csv')
+
+union = pd.read_csv('union.csv')
+print(union)
+
+pd.value_counts(union['lead_type'])
+print(pd.value_counts(union['lead_type']))
+
+# Hay 24 Call y 14 Form
